fd6830e2b404f1de2a5050826edabf5f7972eb11
gabrela framco <gabrielafranco2018@outlook.com>
2023-05-17 11:34:48 -0300

    aula

--- src/public/index.php
+++ src/public/index.php
@@ -11,9 +11,9 @@ echo '<br />';
 $ultimo_nome = "Lourdes";
 echo $ultimo_nome;
 ///$time = array('Maria', 'José','Ana');
-echo $time[2];
+///echo $time[2];
 echo '<br />';
-print_r($time);
+///print_r($time);
 echo 'a: [' . TRUE . ']<br />';
 echo 'a: [' . FALSE . ']<br />';
 echo 'a: [' . (20 > 9) . ']<br />';
@@ -29,3 +29,53 @@ if ($saldo<100){
 }
 
 echo $saldo;
+
+$vegetais = array(
+    'jerimum'=>'https://fsm2009amazonia.org.br/wp-content/uploads/2020/04/abobora-jerimum-1068x601.jpg',
+    'batata' =>'https://www.mundoboaforma.com.br/wp-content/uploads/2015/02/batata.jpg',
+    'tomate'=>'https://pixnio.com/free-images/2017/09/19/2017-09-19-07-03-23.jpg');
+
+$estoqueCarros = array(
+    array('Palio',10,125),
+    array('Gol',15,200),
+    array('Civic',4,38)
+);
+
+echo 'Temos:' . $estoqueCarros[1] [1] . '' . $estoqueCarros[1] [0] . '<br />Vendidos:' . $estoqueCarros[1] [2];
+
+///print_r ($GLOBALS);
+///print_r ($_SERVER);
+print_r ($_GET);
+
+
+?>
+
+<form method="get" action="index.php">
+Nome: <input type="text" name="nome" />
+<input type="submit">
+</form>
+
+<?php
+if(empty($_GET['nome'])){
+    echo 'Nome é um campo obrigatório.';
+}else{
+    echo $_GET['nome'] . ', obrigado por se escrever.';
+}
+?>
+
+<?php
+date_default_timezone_set('America/Los_Angeles');
+$data = date('d/m/Y-');
+$hora = date('H');
+echo $data . $hora;
+if($hora<12){
+    $mensagem = "Bom dia!";
+}elseif(date('H') > 11 && date ('H') < 18){
+    $mensagem = "Boa Tarde!";
+    $mensagem .=' Aceita uma chá gelado?'; 
+}elseif(date('H') > 17){
+    $mensagem = "Boa noite!";
+}
+///phpinfo();
+?>
+Olá! <?php echo $mensagem; ?>
